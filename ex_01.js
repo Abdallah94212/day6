7cb4e360d976f2c9ccdc6dfde4df0a2f4eabbbde
Abdallah94212 <abdallah.borji@epitech.digital>
2024-10-07 14:49:28 +0200

    Create ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,14 @@
+const text = `My name is ${agent.lastName}, ${agent.firstName} ${agent.lastName}! I'm the agent ${agent.code} and I'm ${agent.age} years old.`;
+let p = document.querySelector('#container p');
+p.innerHTML = text   
+}
+var agent = {
+firstName: "James",
+lastName: "Bond",
+code: "007",
+age: 57
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    displayAgentInfo (agent);
+});
